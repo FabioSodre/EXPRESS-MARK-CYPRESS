3185d94b6862bd9d7ceb8589b1b2a04703d509db
unknown <fabiosodre1994@gmail.com>
2023-03-03 12:11:45 -0300

    Melhoria na chamada das rotas da api

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:8080/',
+    env: {
+      apiUrl: 'http://localhost:3333/'
+    },
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,7 +25,7 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('createTask', (taskName = '') => {
-    cy.visit('http://localhost:8080')
+    cy.visit('/')
 
     cy.get('input[placeholder="Add a new Task"]').as('inputTask')
 
@@ -39,7 +39,7 @@ Cypress.Commands.add('createTask', (taskName = '') => {
 
 Cypress.Commands.add('removeTaskByName', (taskName) => {
     cy.request({
-        url: 'http://localhost:3333/helper/tasks',
+        url: Cypress.env('apiUrl') + 'helper/tasks',
         method: 'DELETE',
         body: { name: taskName }
     }).then(responde => {
@@ -49,7 +49,7 @@ Cypress.Commands.add('removeTaskByName', (taskName) => {
 
 Cypress.Commands.add('postTask', (task) => {
     cy.request({
-        url: 'http://localhost:3333/tasks',
+        url: Cypress.env('apiUrl') + 'tasks',
         method: 'POST',
         body: task
     }).then(response => {
